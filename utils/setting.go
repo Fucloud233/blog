80bc6dd51651797ee70b430e4629b75a05931b97
Fucloud <270928474@qq.com>
2023-03-23 14:23:51 +0800

    初始化 后端项目文件 实现最基本的后端api调用

---
+++ utils/setting.go
@@ -0,0 +1,43 @@
+package utils
+
+import (
+	"fmt"
+	"gopkg.in/ini.v1"
+)
+
+var (
+	AppMode  string
+	HttpPort string
+
+	Db         string
+	DbHost     string
+	DbPort     string
+	DbUser     string
+	DbPassWord string
+	DbName     string
+)
+
+// init()函数先于main函数执行
+func init() {
+	file, err := ini.Load("config/config.ini")
+	if err != nil {
+		fmt.Println("配置文件错误，请检查文件路径\n", err)
+	}
+
+	LoadServer(file)
+	LoadData(file)
+}
+
+func LoadServer(file *ini.File) {
+	AppMode = file.Section("server").Key("AppMode").MustString("default")
+	HttpPort = file.Section("server").Key("HttpPort").MustString("3000")
+}
+
+func LoadData(file *ini.File) {
+	Db = file.Section("database").Key("Db").MustString("mysql")
+	DbHost = file.Section("database").Key("DbHost").MustString("localhost")
+	DbPort = file.Section("database").Key("DbPort").MustString("3306")
+	DbUser = file.Section("database").Key("DbUser").MustString("root")
+	DbPassWord = file.Section("database").Key("DbPassWord").MustString("123456")
+	DbName = file.Section("database").Key("DbName").MustString("ginblog")
+}
