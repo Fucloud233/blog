d0e49ec4b4d7f29861ea55fbf11978ddcaa0fc74
Fucloud <270928474@qq.com>
2023-04-10 22:22:23 +0800

    优化 用户相关操作

--- api/v1/user.go
+++ api/v1/user.go
@@ -8,12 +8,12 @@ import (
 	"strconv"
 )
 
-// 查询用户是否存在
+// UserExist 查询用户是否存在
 func UserExist(c *gin.Context) {
 
 }
 
-// 添加用户
+// AddUser 添加用户
 func AddUser(c *gin.Context) {
 	var data model.User
 	_ = c.ShouldBindJSON(&data)
@@ -35,7 +35,7 @@ func AddUser(c *gin.Context) {
 
 // 查询单个用户
 
-// 查询用户列表
+// GetUsers 查询用户列表
 func GetUsers(c *gin.Context) {
 	pageSize, _ := strconv.Atoi(c.Query("pagesize"))
 	pageNum, _ := strconv.Atoi(c.Query("pagenum"))
@@ -58,12 +58,30 @@ func GetUsers(c *gin.Context) {
 	})
 }
 
-// 编辑用户
+// EditUser 编辑用户
 func EditUser(c *gin.Context) {
+	var data model.User
+	id, _ := strconv.Atoi(c.Param("id"))
+	_ = c.ShouldBindJSON(&data)
 
+	code := model.CheckupUser(id, data.Username)
+	if code == errmsg.SUCCESS {
+		model.EditUser(id, &data)
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"message": errmsg.GetErrMsg(code),
+	})
 }
 
-// 删除用户
+// DeleteUser 删除用户
 func DeleteUser(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+	code := model.DeleteUser(id)
 
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"message": errmsg.GetErrMsg(code),
+	})
 }
--- model/User.go
+++ model/User.go
@@ -14,7 +14,7 @@ type User struct {
 	// 为了保证能够存加密值
 	Password string `gorm:"type: varchar(60); not null" json:"password"`
 	Role     int    `gorm:"type: int" json:"role"`
-	// Avator string
+	// Avatar string
 }
 
 func CheckUser(name string) (code int) {
@@ -28,7 +28,21 @@ func CheckUser(name string) (code int) {
 	return errmsg.SUCCESS
 }
 
-// 新增用户 返回一个code
+func CheckupUser(id int, name string) (code int) {
+	var user User
+	db.Select("id, username").Where("username = ?", name).First(&user)
+
+	// 当该用户名是其本身 返回SUCCESS
+	if user.ID == uint(id) {
+		return errmsg.SUCCESS
+	} else if user.ID > 0 {
+		// 该用户名已经被使用
+		return errmsg.ERROR_USERNAME_USED
+	}
+	return errmsg.SUCCESS
+}
+
+// CreateUser 新增用户 返回一个code
 func CreateUser(data *User) int {
 	// 添加密码加
 	data.Password = bcryptPw(data.Password)
@@ -50,6 +64,30 @@ func GetUsers(pageSize int, pageNum int) []User {
 	}
 
 	return users
+}
+
+// DeleteUser 删除用户
+func DeleteUser(id int) int {
+	var user User
+	err := db.Delete(&user, id).Error
+	if err != nil {
+		return errmsg.ERROR_USER_NOT_EXSIT
+	}
+
+	return errmsg.SUCCESS
+}
+
+// EditUser 修改用户
+func EditUser(id int, data *User) int {
+	var user User
+	var maps = make(map[string]interface{})
+	maps["role"] = data.Role
+	maps["username"] = data.Username
+	err = db.Model(&user).Where("id = ? ", id).Updates(maps).Error
+	if err != nil {
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCESS
 }
 
 // bcryptPw 密码加密
